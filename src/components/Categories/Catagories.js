7ecf80c88bb994a596fc54bfab80008be7ec949b
Owali Ullah Shawon <33037708+OwaliShawon@users.noreply.github.com>
2021-11-22 17:59:15 +0600

    created react app

---
+++ src/components/Categories/Catagories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Catagories = () => {
+    return (
+        <div>
+            <p>Catagories</p>
+        </div>
+    );
+};
+
+export default Catagories;
---
+++ src/components/Products/Products.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Products = () => {
+    return (
+        <div>
+            <p>These are products</p>
+        </div>
+    );
+};
+
+export default Products;<p>These are products</p>
